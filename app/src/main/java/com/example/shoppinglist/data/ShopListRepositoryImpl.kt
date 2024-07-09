0601a79855f4aefcb6b22a87c3f11df3ac9a23f7
abl <ablay.yedilbayev@kaspi.kz>
2024-07-09 20:40:45 +0500

    add auto refreshing

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopItem.Companion.UNDEFINED_ID
 import com.example.shoppinglist.domain.ShopListRepository
@@ -9,6 +11,7 @@ object ShopListRepositoryImpl : ShopListRepository {
 	private val shopList = mutableListOf<ShopItem>()
 	private var autoIncrementId = 0
 
+	private val shopListLiveData = MutableLiveData<List<ShopItem>>()
 	init {
 		for (i in 0 until 10) {
 			val item = ShopItem("Name $i", i, true)
@@ -21,16 +24,19 @@ object ShopListRepositoryImpl : ShopListRepository {
 			shopItem.id = autoIncrementId++
 		}
 		shopList.add(shopItem)
+		updateList()
 	}
 
 	override fun editShopItem(shopItem: ShopItem) {
 		val oldElement = getShopItem(shopItem.id)
 		shopList.remove(oldElement)
 		shopList.add(shopItem)
+		updateList()
 	}
 
 	override fun deleteShopItem(shopItem: ShopItem) {
 		shopList.remove(shopItem)
+		updateList()
 	}
 
 	override fun getShopItem(id: Int): ShopItem {
@@ -38,7 +44,11 @@ object ShopListRepositoryImpl : ShopListRepository {
 			?: throw RuntimeException("Element with id: $id not found")
 	}
 
-	override fun getShopList(): List<ShopItem> {
-		return shopList.toList()
+	override fun getShopList(): LiveData<List<ShopItem>> {
+		return shopListLiveData
+	}
+
+	private fun updateList() {
+		shopListLiveData.value = shopList.toList()
 	}
 }
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var viewModel: MainViewModel
+	private var count = 0
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -24,7 +25,11 @@ class MainActivity : AppCompatActivity() {
 		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 		viewModel.shopList.observe(this) {
 			Log.d("MainActivityTest", it.toString())
+			if (count == 0) {
+				count++
+				val item = it[0]
+				viewModel.changeEnableState(item)
+			}
 		}
-		viewModel.getShopList()
 	}
 }
