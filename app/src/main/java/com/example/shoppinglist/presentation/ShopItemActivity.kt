4400768cb3b80a2e22a71347917e87fc80eb042b
abl <ablay.yedilbayev@kaspi.kz>
2024-07-13 12:32:55 +0500

    create ShopItemViewModel.kt

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist.presentation
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.shoppinglist.R
+
+class ShopItemActivity : Activity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_shop_item)
+	}
+}
