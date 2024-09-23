fcc95fa8bb97eb21c4b2a06e270575c1fe7207ab
abl <ablay.yedilbayev@gmail.com>
2024-09-23 09:04:46 +0500

    add content provider

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shoppinglist.presentation
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -48,6 +49,13 @@ class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedList
 				launchFragment(ShopItemFragment.newInstanceAddItem())
 			}
 		}
+		contentResolver.query(
+			Uri.parse("content://com.example.shoppinglist/shop_items"),
+			null,
+			null,
+			null,
+			null
+		)
 	}
 
 	override fun onEditingFinished() {
