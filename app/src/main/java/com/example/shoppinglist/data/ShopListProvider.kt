ac84ba77fef30c10a6bd2a5bbfa827a35e6f702d
abl <ablay.yedilbayev@gmail.com>
2024-09-24 08:26:07 +0500

    add Uri matcher

--- app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
@@ -11,9 +11,11 @@ class ShopListProvider : ContentProvider() {
 
     companion object {
         private const val GET_SHOP_ITEMS_QUERY =100
+        private const val GET_SHOP_ITEM_BY_ID_QUERY =101
     }
     private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
-        addURI("com.example.shoppinglist", "shop_items/*", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.shoppinglist", "shop_items/#", GET_SHOP_ITEM_BY_ID_QUERY)
     }
 
     override fun onCreate(): Boolean {
@@ -29,9 +31,8 @@ class ShopListProvider : ContentProvider() {
     ): Cursor? {
         val code = uriMatcher.match(uri)
         when (code) {
-            GET_SHOP_ITEMS_QUERY -> {
-
-            }
+            GET_SHOP_ITEMS_QUERY -> {}
+            GET_SHOP_ITEM_BY_ID_QUERY -> {}
         }
         log("query uri=$uri, code=$code")
         return null
